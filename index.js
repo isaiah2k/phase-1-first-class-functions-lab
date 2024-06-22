21fe3b84439b01aeb10795163017935d578456a1
Isaiah <zaymgmt707@gmail.com>
2024-06-21 23:52:38 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+const drivers = (['Antonia', 'Nuru', 'Amari', 'Mo'])
+const returnFirstTwoDrivers = drivers => [drivers[0], drivers[1]]
+const returnLastTwoDrivers = drivers => [drivers[2], drivers[3]]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = multiplier => fare => fare * multiplier
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+const selectDifferentDrivers = (drivers, selectingFunction) => selectingFunction(drivers)
