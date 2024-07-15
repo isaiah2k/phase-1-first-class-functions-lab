b243be69560fbf66fafb86b9ed59d5363f7b1a5d
Isaiah <zaymgmt707@gmail.com>
2024-07-15 13:56:59 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const drivers = (['Antonia', 'Nuru', 'Amari', 'Mo'])
+const drivers = (['Antonia', 'Nuru', 'Amari', 'Mo']) 
+// Function to return the first two drivers from the array
 const returnFirstTwoDrivers = drivers => [drivers[0], drivers[1]]
+// Function to return the last two drivers from the array
 const returnLastTwoDrivers = drivers => [drivers[2], drivers[3]]
+// Function for selecting drivers
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
-const createFareMultiplier = multiplier => fare => fare * multiplier
+// Function to make a fare multiplier
+const createFareMultiplier = (multiplier) => {
+  // Returns fare multiplied by specified multiplier
+  return (fare) => {
+  return multiplier * fare
+  }
+}
+// Function to double fare
 const fareDoubler = createFareMultiplier(2)
+// Function to triple fare
 const fareTripler = createFareMultiplier(3)
-const selectDifferentDrivers = (drivers, selectingFunction) => selectingFunction(drivers)
+// Function to select different drivers from the array using a specified function
+const selectDifferentDrivers = (arrayOfDrivers, fn) => fn(arrayOfDrivers);
